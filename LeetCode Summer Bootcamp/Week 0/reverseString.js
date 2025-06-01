e8b332e3950581d70a021300a897269771edd8af
LeslieSuh <lesliejysuh@gmail.com>
2025-06-01 19:39:05 +0100

    week 0 easy question

---
+++ LeetCode Summer Bootcamp/Week 0/reverseString.js
@@ -0,0 +1,31 @@
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+process.stdin.on("end", (_) => {
+  inputString = inputString.trim().split("\n");
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+// Function to be implemented
+function reverseString(s) {
+  // Write your code here
+  return s.split("").reverse().join("");
+}
+
+function main() {
+  const s = readLine();
+  const result = reverseString(s);
+  console.log(result);
+}
