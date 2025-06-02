5a46f5412cbef4e5e2e2a3665b7a6102f7a59491
LeslieSuh <lesliejysuh@gmail.com>
2025-06-02 21:52:52 +0100

    Find the Index of the First Occurence in a String

--- LeetCode Summer Bootcamp/Week 0/1.js
+++ LeetCode Summer Bootcamp/Week 0/1.js
@@ -1,1 +1,45 @@
 // Find the Index of the First Occurence in a String
+
+"use strict";
+
+const fs = require("fs");
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+// Read input
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+process.stdin.on("end", (_) => {
+  inputString = inputString.trim().split("\n");
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+// Function to be implemented
+function strStr(haystack, needle) {
+  // Write your code here
+  const i = haystack.length;
+  const j = needle.length;
+  for (let k = 0; k <= i - j; k++) {
+    if (haystack.substring(k, k + j) === needle) {
+      return k;
+    }
+  }
+  return -1;
+}
+
+function main() {
+  const haystack = readLine();
+  const needle = readLine();
+
+  const result = strStr(haystack, needle);
+  console.log(result);
+}
