7860a235f5cd1e85614a23f556a1f5eea621f33f
LeslieSuh <lesliejysuh@gmail.com>
2025-06-16 16:29:03 +0100

    search insertion position code added

--- LeetCode Summer Bootcamp/Week 2/1.js
+++ LeetCode Summer Bootcamp/Week 2/1.js
@@ -1,1 +1,18 @@
 // Search Insert Position (You must write an algorithm with O(log n) runtime complexity)
+
+function searchInsertPosition(nums, target) {
+  let start = 0;
+  let end = nums.length - 1;
+
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      start = mid + 1;
+    } else if (nums[mid] > target) {
+      end = mid - 1;
+    }
+  }
+  return start;
+}
