58486f640ed28b84d519aee975fff80218323c40
LeslieSuh <lesliejysuh@gmail.com>
2025-06-22 17:26:30 +0100

    cutting ribbons - need to be revised

--- LeetCode Summer Bootcamp/Week 2/5.js
+++ LeetCode Summer Bootcamp/Week 2/5.js
@@ -1,1 +1,22 @@
 // Cutting Ribbons
+
+function maxLength(ribbons, k) {
+  let left = 1;
+  let right = Math.max(...ribbons);
+  let answer = 0;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    let count = 0;
+    for (let ribbon of ribbons) {
+      count += Math.floor(ribbon / mid);
+    }
+    if (count >= k) {
+      answer = mid;
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return answer;
+}
