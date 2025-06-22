6f439ff419bee20721b8646636bb83c2dd851e17
LeslieSuh <lesliejysuh@gmail.com>
2025-06-22 17:12:45 +0100

    find peak element

--- LeetCode Summer Bootcamp/Week 2/3.js
+++ LeetCode Summer Bootcamp/Week 2/3.js
@@ -1,1 +1,16 @@
 // Find Peak Element
+
+function findPeakElement(nums) {
+  // Write your code here
+  let left = 0;
+  let right = nums.length - 1;
+  while (left < right) {
+    const mid = Math.floor((left + right) / 2);
+    if (nums[mid] < nums[mid + 1]) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+  return left;
+}
